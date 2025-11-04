2c5c0886cbb580bf76c9cca521ebca01200c969d
kirshmatvey <matveykirshchin01@gmail.com>
2025-11-05 00:47:35 +0400

    task 3 hw3 done

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,5 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(firstInteger, secondInteger, thirdInteger) {
+    return Math.max(firstInteger, secondInteger, thirdInteger);
+}
