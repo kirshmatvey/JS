a0f2f853dd0261c05203dff9fcb7df29a4824f5f
kirshmatvey <matveykirshchin01@gmail.com>
2025-11-17 02:23:42 +0400

    made a mapping function

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,11 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+const map = (array, callback) => {
+    const newArray = []
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index]
+        newArray.push(callback(element, index));
+    }
+    return newArray;
+}
